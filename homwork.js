81d91242b22d0907eaa13689815eadb7420c7531
zohidbe <zohaxaydarov@gmail.com>
2024-08-03 01:04:55 +0500

    uy ishi end

---
+++ homwork.js
@@ -0,0 +1,21 @@
+
+let alisherPul = prompt("Qancha puli borligini so'mda kiriting:");
+
+let biletDollarda = 500;
+let mehmonxonaDollarda = 250;
+let kongilocharYevroda = 120;
+
+let dollarKursi = 11000.34;
+let yevroKursi = 12354.03;
+
+let biletSomda = biletDollarda * dollarKursi;
+let mehmonxonaSomda = mehmonxonaDollarda * dollarKursi;
+let kongilocharSomda = kongilocharYevroda * yevroKursi;
+
+let umumiyHarajatlar = biletSomda + mehmonxonaSomda + kongilocharSomda;
+
+if (alisherPul >= umumiyHarajatlar) {
+    console.log("Oq yo’l, Alisher!");
+} else {
+    console.log("sizni pulingiz yetmaydi hurmatli mijoz.");
+}
---
+++ main.js
@@ -0,0 +1,51 @@
+// let num = 11;
+
+// if ( num > 0) {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// };
+
+
+// let a = 1;
+
+// let b = 2;
+
+// let c = 3;
+
+// if (a <= b && b<= c ){
+//   console.log(true)  
+// }
+
+
+
+
+// let USD = 12600;
+// let EURO = 13600;
+
+// let avia = 500;
+
+// let hotel = 250;
+
+// let a = 120;
+
+// console.log((avia))
+
+// let balace = prompt("pulingizni kiriting")
+
+// console.log(balace);
+
+
+// juft toq
+
+
+// let random = Math.round(Math.random() * 100)
+
+// if ( random % 2 == 0) {
+//     console.log(random, 'juft son');
+// }else {
+//     console.log(random, 'toq son');
+// }
+
